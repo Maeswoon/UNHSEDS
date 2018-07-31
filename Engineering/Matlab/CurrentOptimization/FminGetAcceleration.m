a8d9f116eb3933a060381fba2b8176724f6686e6
reillywebb <webbreilly@gmail.com>
2018-07-30 22:56:04 -0400

    jfsdfs

--- Engineering/Matlab/CurrentOptimization/FminGetAcceleration.m
+++ Engineering/Matlab/CurrentOptimization/FminGetAcceleration.m
@@ -3,7 +3,7 @@
 
 function [a, d] = FminGetAcceleration (t , v, h, x)
 g = 9.81;
-f = GetThrust ( t );
+f = GetThrust ( t,x );
 m = GetMass ( t,x ); %THIS WILL CHANGE WITH ITERATIONS
 [d] = FminGetDrag ( v,h,t,x );
 a = ( f - m*g - d )/ m ; % Newton Second Law

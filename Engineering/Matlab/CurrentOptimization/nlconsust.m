1b9c68fe14872ad67a62bc69bfcdd8a70aac6721
cjnitschelm <cjn1012@wildcats.unh.edu>
2018-08-13 20:51:39 -0400

    more on CGCP

--- Engineering/Matlab/CurrentOptimization/nlconsust.m
+++ Engineering/Matlab/CurrentOptimization/nlconsust.m
@@ -26,50 +26,41 @@ xdrogueparachute      = 0.10;    % From aft of E-Bay Bulk Plate
 xboosterparachute     = 0.12;    % From aft of Coupler Bulk Plate
 
 Dbattery            = Debay - Lebay/2 + xbattery + Lbattery/2;       % Offset from E-Bay Forward Bulk Plate 
-Ddrogueparachute    = Debay + Lebay/2 + xdrogueparachute + Ldrogueparachute/2 - .07;                                                                % Offset from Nosecone Forward
+Ddrogueparachute    = Debay + Lebay/2 + xdrogueparachute + Ldrogueparachute/2;                                                                % Offset from Nosecone Forward
 Dmainparachute      = x(1) + xmainparachute + Lmainparachute/2;                                                % Offset from E-bay Aft
 Dboosterparachute   = Dstagingcoupler + LcouplerBodyTubeStage/2 + x(4)/2 + xboosterparachute + Lboosterparachute/2;   % Offset from Staging Coupler Aft
-Diameter=.0574;
-Thickness=.0016;
+Diameter=Part.Nosecone.diameter;   % GET THE RIGHT PART FOR DIAMETER INSERT
+Thickness=.00175;
 Rbodytube = Diameter/2;     % Radius of Bodytube
-finthickness=.00321;
-rhocf=1800; %kg/m3
-rhobt=1120;
-rhofg=1500;
 
-Mnosecone        = .113; %THESE SHOULD ALSO CHANGE WITH LENGTH, use density ***********
-Mshoulder        = pi*x(2)*(Rbodytube^2-(Rbodytube-Thickness)^2)*rhobt;
-Mebay            = .401; % With a payload of 4 bolts at 180 grams total
-Msustbodytube    = pi*x(3)*(Rbodytube^2-(Rbodytube-Thickness)^2)*rhobt;
-Mforwardfins     = .030;
-Mstagingcoupler  = .083;    
-Mboosterbodytube = .114;    
-Maftfins         = .030;   
-Mboostinit       = .194 + .077;    % Initial mass of booster
-Msustinit        = .198 + .081;    % Initial mass of sustainer 
-Mboostprop       = .082;    % Mass of total propellant
-Msustprop        = .087;    % Mass of total propellant
-% Internal electronics (kg)  
-Mbattery       = 0.045;      
-Maltimeter     = 0.000;
-MSEDSaltimeter = 0.000;
-Mswitch        = 0.000;
+% Linear Weights
+PL = 4.882; % 4882 grams per square meter for Plate Parts
+BT = .330;  % 330 grams per meter for Body Tube
+CP = .322;  % 322 grams per meter for Coupler Body Tube
+ET = .168;  % 168 grams per meter for Engine Tube
+
+
+Maftfins         = PL * .5*x(12)*x(13)*x(14); 
+Mforwardfins     = PL * .5*x(8)*x(9)*x(10);
+Mnosecone        = .200; % Needs to be accuratly estimated with shoulder factored in
+Mebay            = .200; % Constant that needs to be measured when built
+Msustbodytube    = x(5)*BT;
+Mstagingcoupler  = .08 * CP;    
+Mboosterbodytube = x(12) * BT;    
+Mboostinit       = .294;   % Initial mass of booster
+Msustinit        = .349;   % Initial mass of sustainer 
 
 % Internal pieces (kg)
-Mdrogueparachute    = .025;
+Mdrogueparachute    = .025;    % MEASURE THIS
 Mmainparachute      = .078;
 Mboosterparachute   = .043;   
 
-Mcenteringring      = 0;
+
 %for now we are optimizing for fixed initial caliber=1.5
-%tb      = linspace(0,Tboost,res);             % 1000 different times of flight from 0 to .7 seconds
-%Mboost  = Mboostinit-(Mboostprop/Tboost)*tb;  % Mass of the booster during flight
+
 CGBoost = Mboostinit*Dboost;                     % Center of Gravity of booster during flight
-
-
 Mtot    = Mboostinit + Msustinit + Mnosecone + Mshoulder + Mebay + Msustbodytube + Mforwardfins + Mstagingcoupler + Mboosterbodytube...               % Total mass of sustainer and
     + Maftfins + Mbattery + Maltimeter + MSEDSaltimeter + Mswitch + Mdrogueparachute + Mboosterparachute + Mmainparachute + 4*Mcenteringring;     % booster through flight
-
 CGboostsust      = (CGBoost + Dnosecone*Mnosecone + Debay*Mebay + Dsustbodytube*Msustbodytube + Dforwardfins*Mforwardfins...         % Center of gravity of entire rocket
         + Dstagingcoupler*Mstagingcoupler + Dsust*Msustinit + Dboosterbodytube*Mboosterbodytube + Daftfins*Maftfins + Mbattery*Dbattery...        % through flight before seperation
         + Mdrogueparachute*Ddrogueparachute + Mboosterparachute*Dboosterparachute...

123ad5a99cd08714217332aa1dcb7b2bffce277e
LucasSimmonds <49700119+LucasSimmonds@users.noreply.github.com>
2019-08-18 21:19:55 -0400

    Working shit.
    Arduino scrips for the flow regulator.

--- Engineering/2018-2019/Electrical Dump/Test/Test.ino
+++ Engineering/2018-2019/Electrical Dump/Test/Test.ino
@@ -3,6 +3,7 @@ int in1 = 3;
 int in2 = 6;
 
 const byte interruptPin = 2;
+int count = 1;
 
 
 void setup() {
@@ -22,6 +23,7 @@ void setup() {
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
+  
 }
 
 void loop() {
@@ -30,27 +32,45 @@ void loop() {
     Serial.print("Waiting for go signal\n");
   }
   
-  else if (digitalRead(10) == HIGH && digitalRead(11) == LOW) {
-    digitalWrite(in1, HIGH);
-    digitalWrite(pwmPin, HIGH);
-    Serial.print("Opening\n");
-    delay(1550);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, LOW);
-    Serial.print("Full Open\n");
-    delay(2500);
-    digitalWrite(9, HIGH);
-    delay(50);
-    digitalWrite(9, LOW);
+  else if (digitalRead(10) == HIGH && digitalRead(11) == LOW ) {
     Serial.print("Igniting\n");
-    delay(180000);
-    digitalWrite(in1, LOW);
-    digitalWrite(in2, HIGH);
-    Serial.print("Closed\n");
-    delay(1500);
+    digitalWrite(9, HIGH);                //Igniter 
+    delay(100);
+    //digitalWrite(9, LOW);
+    delay(3400);                          //Delay for burn
+
+    // HALF OPEN
+    digitalWrite(in1, HIGH);              //Direction --> Reverse
+    analogWrite(pwmPin, 255);             //digitalWrite(pwmPin, HIGH);           //Speed
+    Serial.print("Opening\n");
+    delay(1300);                          //For how long
     digitalWrite(in1, LOW);
     digitalWrite(in2, LOW);
-    //counter++;
+    delay(10000);
+    
+    // FULLY OPEN
+    Serial.println("Done Opening");                                       //Serial.print("Full Open\n");
+    digitalWrite(in1, HIGH);
+    analogWrite(pwmPin, 255);
+    delay(600);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+
+    //FULLY CLOSING
+    digitalWrite(in2, HIGH);
+    analogWrite(pwmPin, 255);
+    delay(2000);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    
+    //delay(180000);
+    //digitalWrite(in1, LOW);
+    //digitalWrite(in2, HIGH);
+    Serial.print("Closed\n");
+    //delay(1500);
+    //digitalWrite(in1, LOW);
+    //digitalWrite(in2, LOW);
+    count++;
   }
 
   while (digitalRead(11) == HIGH) {

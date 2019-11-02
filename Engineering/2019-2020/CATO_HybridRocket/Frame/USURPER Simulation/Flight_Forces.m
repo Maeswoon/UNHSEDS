a0d3a39d253e338439e4df1584eccc043da73d7e
dto1001 <55600545+dto1001@users.noreply.github.com>
2019-11-02 13:29:07 -0400

    drag coding

--- Engineering/2019-2020/CATO_HybridRocket/Frame/USURPER Simulation/Flight_Forces.m
+++ Engineering/2019-2020/CATO_HybridRocket/Frame/USURPER Simulation/Flight_Forces.m
@@ -18,12 +18,22 @@ accel1=f_thrust/massvec;
 
 % nose cone
 cdnose=.15; %value chosen, we need to do more research!!!!!
-roeair=linspace(1.225,.9093); %density vector from 0 to 3000 meters kg/m^3
-dia=7*.0254; % meters
+rhoair=linspace(1.225,.9093); %density vector from 0 to 3000 meters kg/m^3
+dia=7*.0254; % diameter of outside of rocket, meters
 vel1= ; % m/s need to determine this thru physics and accel1 vec!!!!!!
-f_nose=.5*cdnose*roeair*(pi*(dia^2)/4)*(vel1^2);
+drag_nose=.5*cdnose*rhoair*(pi*(dia^2)/4)*(vel1^2);
 
 % skin friction force
+length=2.15; % length of rocket in meters
+dynvisc=1.198e-5;
+Re1=(roeair*vel1*length)/dynvisc; % reynolds number, unitless
+cfskin=1/((1.5*log(Re1)-5.6)^2); % friction coeff, unitless
+cylsurf=pi*dia*length; % surface area outside of rocket, meters
+finsurf= ; % surface area of one fin
+fins=4; % number of fins on rocket
+finsurftot= ; % surface area of all fins
+totsurf=cylsurf+finsurftot; % total surface area of rocket
+drag_skin=.5*cfskin*rhoair*totsurf*(vel1^2); 
 
 % base force (vaccum created under rocket when combustion is over)
 

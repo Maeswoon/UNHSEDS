5e624dea8859e1d9fb7c0827bbc280d7228dbf03
bml1022 <50719001+bml1022@users.noreply.github.com>
2020-01-30 12:02:01 -0500

    Update Hardwired_V2_Ignition.ino

--- Engineering/2019-2020/USURPER_HybridRocket/Propulsion/Avioinic Propulsion/Flow Regulation/Hardwired_V2_Ignition/Hardwired_V2_Ignition.ino
+++ Engineering/2019-2020/USURPER_HybridRocket/Propulsion/Avioinic Propulsion/Flow Regulation/Hardwired_V2_Ignition/Hardwired_V2_Ignition.ino
@@ -1,7 +1,7 @@
 int vent = 5;   // Vent solenoid digital pin 5
 int flow = 4;   // Flow solenoid digital pin 4
 int lswitch = 10; // Launch Switch to digital 10
-int button = 11; // Abort launch to digital 11
+int button = 12; // Abort launch button to digital 12
 int igniter = 9; // Igniter on digital 9
 
 const byte interruptPin = 2;
@@ -33,13 +33,15 @@ void loop() {
   else if (digitalRead(lswitch) == HIGH && digitalRead(button) == LOW ) {
     Serial.print("Igniting\n");
     digitalWrite(igniter, HIGH);           //Ignite E-match 
-    delay(3500);                          //Delay for burn
+    //delay(3500);                          //Delay for burn
+    delay(1000);
     digitalWrite(igniter, LOW);
 
     // OPEN SOLENOID
     digitalWrite(flow, HIGH);           //Flow Solenoid Open
     Serial.print("Flow Open\n");
-    delay(180000);
+    //delay(180000);
+    delay(5000);
     
     // CLOSE SOLENOID
     digitalWrite(flow, LOW);            //Flow Solenoid Closed
@@ -49,7 +51,8 @@ void loop() {
   else if (digitalRead(button) == HIGH ) {
     digitalWrite(vent, HIGH);           //Vent Solenoid Open
     Serial.print("Aborting Launch\n");
-    delay(180000);
+    //delay(180000);
+    delay(5000);
     digitalWrite(vent, LOW);
   }
   }

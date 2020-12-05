489a7f1491966df33cd183de9cea4db37aad6177
RThyne1015 <42524743+RThyne1015@users.noreply.github.com>
2020-12-05 18:08:41 -0500

    heat

---
+++ Engineering/2020-2021/UNAMED_Rocket2021/Propulsion/Banshee V1/Heat Evalualtions/Heat_Analysis.m
@@ -0,0 +1,14 @@
+clear all; close all;
+
+% Purpose is to Evaluate different methods of evaluating the heat transfer
+% and thermal insulation of our hybrid rocket engine
+
+Tin= ; %K Temperature inside the chamber.
+Tout= ; %K Ambient Temperature
+
+R1= %; Inner Radius of Insulator
+R2= %; Inner Radius of Chamber Wall
+R3= %; Outer Radius of Chamber Wall
+
+kI= % Coefficient of conduction for Insulator Material
+kC  % Coefficient of conduction for Chamber Material
